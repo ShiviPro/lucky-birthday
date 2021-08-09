751374644b9464f3ce8e8c6d7899651e14733e5b
Shivam Tewari <www.shivamtewari@gmail.com>
2021-08-09 10:23:33 +0530

    style: clicking on link, shows form with close btn

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,21 +1,66 @@
-let dob = document.querySelector("#dob__input");
-let luckyNo = document.querySelector("#lucky-num__input");
-let checkBtn = document.querySelector("#check-btn");
+let contentDiv = document.querySelector("#content");
 
-checkBtn.addEventListener("click", (event) => {
-  let dobVal = dob.value;
-  let luckyNoVal = parseInt(luckyNo.value);
-  let numbers = "0123456789";
-  let digitSum = 0;
-  for (let charIndex = 0; charIndex < dobVal.length; charIndex++) {
-    let char = dobVal.charAt(charIndex);
+let form = document.createElement("div");
+form.classList.add("form");
+form.innerHTML = `
+<svg class="close-btn" id="close-btn">
+    <use xlink:href="#close">
+</svg>
+<div class="dob">
+    <label class="dob__heading" for="dob">Select your date of birth</label>
+    <br />
+    <input class="dob__input" id="dob__input" type="date" name="dob" />
+</div>
 
-    if (numbers.indexOf(char) > -1) {
-      let digit = parseInt(char);
-      digitSum += digit;
+<div class="lucky-num">
+    <label class="lucky-num__heading" for="lucky-num"
+    >Enter your lucky number</label
+    >
+    <br />
+    <input
+        class="lucky-num__input"
+        id="lucky-num__input"
+        type="number"
+        name="lucky-num"
+    />
+</div>
+
+<button class="check-btn" id="check-btn">Check</button>
+`;
+
+let formLink = document.querySelector("#form-link");
+
+let heroText = document.querySelector("#hero-txt");
+
+formLink.addEventListener("click", (event) => {
+  event.preventDefault();
+  heroText.style.display = "none";
+  contentDiv.appendChild(form);
+  let dob = document.querySelector("#dob__input");
+  let luckyNo = document.querySelector("#lucky-num__input");
+  let checkBtn = document.querySelector("#check-btn");
+
+  checkBtn.addEventListener("click", (event) => {
+    let dobVal = dob.value;
+    let luckyNoVal = parseInt(luckyNo.value);
+    let numbers = "0123456789";
+    let digitSum = 0;
+    for (let charIndex = 0; charIndex < dobVal.length; charIndex++) {
+      let char = dobVal.charAt(charIndex);
+
+      if (numbers.indexOf(char) > -1) {
+        let digit = parseInt(char);
+        digitSum += digit;
+      }
     }
-  }
-  if (digitSum % luckyNoVal === 0) {
-    alert('Your Birthday is "LUCKY" ');
-  }
+    if (digitSum % luckyNoVal === 0) {
+      alert('Your Birthday is "LUCKY" ');
+    }
+  });
+  let closeBtn = document.querySelector("#close-btn");
+
+  closeBtn.addEventListener("click", (event) => {
+    heroText.style.display = "block";
+    form.remove();
+  });
 });
